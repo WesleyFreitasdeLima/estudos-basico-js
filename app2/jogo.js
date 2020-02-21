73a0a428cadc6386c6241ef0ccab73941275e725
Wesley Freitas de Lima <w.freitas.lima@gmail.com>
2020-02-21 05:43:48 -0300

    App Game Mata Mosquito - Definindo a dimensão do palco do jogo

---
+++ app2/jogo.js
@@ -0,0 +1,10 @@
+let altura, largura;
+
+function ajustaTamanhoPalcoJogo() {
+    altura = window.innerHeight;
+    largura = window.innerWidth;
+
+    console.log(largura, altura)
+}
+
+ajustaTamanhoPalcoJogo();
